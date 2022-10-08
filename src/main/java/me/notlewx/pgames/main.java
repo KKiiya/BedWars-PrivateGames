e11c77e66b7fd11b961f33be8d97e4fbc778a927
NotLewUwU <abderrahmanlabiod@iessantiagohernadez.com>
2022-10-08 21:48:38 +0200

    First commit
    This is to prepare the workspace when I start working in it

---
+++ src/main/java/me/notlewx/pgames/main.java
@@ -0,0 +1,17 @@
+package me.notlewx.pgames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
