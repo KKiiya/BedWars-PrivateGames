7085621fba1c51a0bf144bbd06d63436cc475538
NotLewUwU <abderrahmanlabiod@iessantiagohernadez.com>
2022-12-28 18:40:03 +0100

    Update MySQL.java

--- src/main/java/me/notlewx/pgames/db/MySQL.java
+++ src/main/java/me/notlewx/pgames/db/MySQL.java
@@ -5,6 +5,7 @@ import me.notlewx.pgames.main;
 import org.bukkit.Bukkit;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import static me.notlewx.pgames.main.bwconfig;
 import static me.notlewx.pgames.main.bwproxy;
@@ -64,5 +65,99 @@ public class MySQL {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+    public void createPlayerData(String path) {
+        Bukkit.getScheduler().runTaskAsynchronously(main.plugin(), () -> {
+            try {
+                Connection c = (main.plugin()).db.getConnection();
+                try {
+                    String sql = "INSERT INTO bw1058_private_games(name, oneHitOneKill, lowGravity, speed, bedInstaBreak, maxTeamUpgrades, allowBreakMap, noDiamonds, noEmeralds, respawnEventTime, healthBuffLevel, eventsTime) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+                    PreparedStatement ps = c.prepareStatement(sql);
+                    ps.setString(1, path);
+                    ps.setBoolean(2, false);
+                    ps.setBoolean(3, false);
+                    ps.setInt(4, 1);
+                    ps.setBoolean(5, false);
+                    ps.setBoolean(6, false);
+                    ps.setBoolean(7, false);
+                    ps.setBoolean(8, false);
+                    ps.setBoolean(9, false);
+                    ps.setInt(10, 1);
+                    ps.setInt(11, 1);
+                    ps.setInt(12, 1);
+                    ps.executeUpdate();
+                    ps.close();
+                    if (c != null)
+                        c.close();
+                } catch (Throwable throwable) {
+                    if (c != null)
+                        try {
+                            c.close();
+                        } catch (Throwable throwable1) {
+                            throwable.addSuppressed(throwable1);
+                        }
+                    throw throwable;
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+    public void setData(String path, boolean torf, String type, int amount) {
+        try {
+            Connection c = (main.plugin()).db.getConnection();
+            try {
+                PreparedStatement ps = c.prepareStatement("UPDATE bw1058_private_games SET " + type + "=? WHERE name=?");
+                ps.setInt(1, amount);
+                ps.setBoolean(2, torf);
+                ps.setString(3, path);
+                ps.executeUpdate();
+                ps.close();
+                if (c != null)
+                    c.close();
+            } catch (Throwable throwable) {
+                if (c != null)
+                    try {
+                        c.close();
+                    } catch (Throwable throwable1) {
+                        throwable.addSuppressed(throwable1);
+                    }
+                throw throwable;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getData(String path, String type) {
+        try {
+            Connection c = (main.plugin()).db.getConnection();
+            try {
+                PreparedStatement ps = c.prepareStatement("SELECT " + type + " FROM bw1058_private_games WHERE name=?");
+                ps.setString(1, path);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    String str = rs.getString(type);
+                    if (c != null)
+                        c.close();
+                    return str;
+                }
+                rs.close();
+                ps.close();
+                if (c != null)
+                    c.close();
+            } catch (Throwable throwable) {
+                if (c != null)
+                    try {
+                        c.close();
+                    } catch (Throwable throwable1) {
+                        throwable.addSuppressed(throwable1);
+                    }
+                throw throwable;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
