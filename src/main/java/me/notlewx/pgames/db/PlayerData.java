bdbe2f99bfbbb73b9e3c58d2bd0f492783b33186
NotLew_x <105589176+NotLewUwU@users.noreply.github.com>
2023-03-29 09:59:59 +0200

    Update PlayerData.java

--- src/main/java/me/notlewx/pgames/db/PlayerData.java
+++ src/main/java/me/notlewx/pgames/db/PlayerData.java
@@ -4,6 +4,8 @@ import me.notlewx.pgames.api.interfaces.IPlayerData;
 import org.bukkit.entity.Player;
 
 public class PlayerData implements IPlayerData {
+    MySQL mySQL = new MySQL();
+    SQLite sqLite = new SQLite();
     @Override
     public Player getPlayer(Player player) {
         return player;
